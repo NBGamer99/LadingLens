4084d3b38b6502392ce2c29c53aaf4ad4644e2a1
NBGamer99 <nabouziyasser80@gmail.com>
2026-01-26 18:03:10 +0100

    feat: Migrating the code to the Pydantic V3

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional
 
 class Settings(BaseSettings):
@@ -14,7 +14,6 @@ class Settings(BaseSettings):
     FIRESTORE_COLLECTION_HBL: str = "hbl"
     FIRESTORE_COLLECTION_MBL: str = "mbl"
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
